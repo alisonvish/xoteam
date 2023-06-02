b64fa9e696584da8857d5d48e2af29399101f95d
Kristina Romanovskaya <124786607+kr27904@users.noreply.github.com>
2023-06-02 13:27:33 +0300

    Update project.py

--- project.py
+++ project.py
@@ -5,3 +5,7 @@ import shutil
 def folders_sort(folder_path):
     subfolders = [f.path for f in os.scandir(folder_path) if f.is_dir()] #вложенный файл
     new_folders = []
+    for i in range(1, 11):
+        new_folder_path = os.path.join(folder_path, str(i) + '_new')
+        os.makedirs(new_folder_path)
+        new_folders.append(new_folder_path)
