0f3175511e8dba0d9ee00250e340d89da0b817ca
alisonvish <118972461+alisonvish@users.noreply.github.com>
2023-06-08 23:50:56 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from docx import 'А.new'
+from docx import 'Б.new'
+from docx import 'В.new'
+from docx import 'Г.new'
+from project import folders_sort
+if __name__ =='__main__':
+  files_to_change = ['А', 'Б', 'В', 'Г']
+  folders_sort(files_to_change)
