89f930f1ec747b546c8e46d7ebef4efe4fda9e01
alisonvish <118972461+alisonvish@users.noreply.github.com>
2023-06-09 00:21:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from docx import 'А.new'
-from docx import 'Б.new'
-from docx import 'В.new'
-from docx import 'Г.new'
+import os
+import shutil
+
 from project import folders_sort
 if __name__ =='__main__':
   files_to_change = ['А', 'Б', 'В', 'Г']
